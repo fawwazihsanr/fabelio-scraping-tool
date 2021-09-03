86118aa5a4062c9a347eefadfc62ba48a3300374
rama <fawwaz.ramadhan@binus.ac.id>
2021-09-03 08:53:47 +0700

    ui fix

--- core/views.py
+++ core/views.py
@@ -1,17 +1,15 @@
 import pdb
 
 from bs4 import BeautifulSoup
-from django.http import JsonResponse
-from django.views.generic import CreateView
 
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.viewsets import ViewSet
 
 from core.repositories import ScraperRepository
 
+from urllib import parse
 import requests
 
 
@@ -19,31 +17,35 @@ import requests
 @permission_classes((AllowAny,))
 def create_submission(request):
     try:
-        header = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'}
-        page = requests.get(request.data['url'], headers=header)
-        soup = BeautifulSoup(page.content, 'html.parser')
+        if parse.urlparse(request.data['url']).hostname == 'fabelio':
+            header = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'}
+            page = requests.get(request.data['url'], headers=header)
+            soup = BeautifulSoup(page.content, 'html.parser')
 
-        name = soup.find(id='product-name').get_text()
-        price = soup.find(id="product-final-price").get_text()
-        url = request.data['url']
-        description = soup.find_all('span', {'class': 's1'}) if soup.find_all('span', {'class': 's1'}) else soup.find_all('em')
-        image = soup.find(id='product-image').get('src')
+            name = soup.find(id='product-name').get_text()
+            price = soup.find(id="product-final-price").get_text()
+            url = request.data['url']
+            description = soup.find_all('span', {'class': 's1'}) if soup.find_all('span', {'class': 's1'}) else soup.find_all('em')
+            image = soup.find(id='product-image').get('src')
 
-        data = {
-            'name': name,
-            'price': price,
-            'url': url,
-            'description': description,
-            'image': image
-        }
-        scraper = ScraperRepository.create(data)
-        return Response(
-            data={'id': scraper},
-            status=status.HTTP_201_CREATED
-        )
+            data = {
+                'name': name,
+                'price': price,
+                'url': url,
+                'description': description,
+                'image': image
+            }
+            scraper = ScraperRepository.create(data)
+            return Response(
+                data={'id': scraper},
+                status=status.HTTP_201_CREATED
+            )
+        else:
+            return Response(
+                data={'message': 'Input fabelio url'},
+                status=status.HTTP_400_BAD_REQUEST)
     except Exception as exp:
-        print(exp)
         return Response(
             data={'message': 'Internal server error'},
             status=status.HTTP_500_INTERNAL_SERVER_ERROR)
@@ -55,7 +57,7 @@ def get_submission(request):
     data = ScraperRepository.read()
     return Response(
         data=data,
-        status=status.HTTP_201_CREATED
+        status=status.HTTP_200_OK
     )
 
 
@@ -65,5 +67,5 @@ def get_submission_by_id(request, id):
     data = ScraperRepository.read_by_id(id)
     return Response(
         data=data,
-        status=status.HTTP_201_CREATED
+        status=status.HTTP_200_OK
     )
